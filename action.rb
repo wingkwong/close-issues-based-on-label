9c8773bfb4e9c50d8f56cb93cf146c7844f23796
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2022-02-17 11:31:32 +0800

    Update action.rb

--- action.rb
+++ action.rb
@@ -2,6 +2,7 @@ require 'octokit'
 
 repo = ENV["GITHUB_REPOSITORY"]
 label = ENV["LABEL"]
+version = ENV["VERSION"]
 
 client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
 client.auto_paginate = true
@@ -9,5 +10,6 @@ client.auto_paginate = true
 open_issues = client.list_issues(repo, { :labels =>  label, :state => 'open'})
 
 open_issues.each do |issue|
+  client.add_comment(repo, issue.number, "Fixed in v#{version}.")
   client.close_issue(repo, issue.number)
 end
